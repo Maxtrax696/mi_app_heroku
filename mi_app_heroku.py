1f43f12c95d13f10ee187dbc5574f3e251343fb2
Maxtrax696 <yoshua.calahorrano1999@gmail.com>
2025-05-21 18:35:10 -0500

    Update mi_app_heroku.py
    Cambio de mensaje

--- mi_app_heroku.py
+++ mi_app_heroku.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hola Mundo con Heroku (PD)"
+    return "Hola Mundo con Heroku (PD) y Usando Actions de GitHub"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # usa $PORT si existe, si no, usa 5000
